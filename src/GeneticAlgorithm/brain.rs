97c7d4b7f2934eac94b59c447fe21e331585bc3c
Lilith645 <lilith@inet-sys.com>
2018-08-19 03:22:30 +1000

    Can launch game through terminal Changes committed: 	modified:   src/GeneticAlgorithm/brain.rs 	modified:   src/main.rs

--- src/GeneticAlgorithm/brain.rs
+++ src/GeneticAlgorithm/brain.rs
@@ -1,9 +1,13 @@
-//pub const P_LEVEL_WEIGHT: f32 =  1;
-//pub const G_LEVEL_WEIGHT: f32 = 0.5;
-
-
-pub fn fitness(player_level: i32, health: i32, actual_level: i32) {
+pub const P_LEVEL_WEIGHT: f32 =  1.0;
+pub const G_LEVEL_WEIGHT: f32 = 0.5;
+pub const P_HEALTH_WEIGHT: f32 = 0.2;
+/*
+pub fn fitness(player_level: i32, health: i32, actual_level: i32) -> i32 {
   let mut score = 0;
   
+  score = (player_level * P_LEVEL_WEIGHT + health * P_HEALTH_WEIGHT) * actual_level;
   
-}
+  score.floor()
+}*/
+
+
