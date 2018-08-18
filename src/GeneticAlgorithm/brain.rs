24c6b09e824877a6f2bb0881739000a5999aa013
Lilith645 <lilith@inet-sys.com>
2018-08-19 07:29:38 +1000

    Added basic scaffolding and structure for genetic algorithm Changes committed: 	modified:   Cargo.toml 	modified:   src/GeneticAlgorithm/brain.rs 	modified:   src/main.rs

--- src/GeneticAlgorithm/brain.rs
+++ src/GeneticAlgorithm/brain.rs
@@ -1,13 +1,177 @@
+use rand;
+use rand::{thread_rng, Rng};
+
 pub const P_LEVEL_WEIGHT: f32 =  1.0;
 pub const G_LEVEL_WEIGHT: f32 = 0.5;
 pub const P_HEALTH_WEIGHT: f32 = 0.2;
-/*
-pub fn fitness(player_level: i32, health: i32, actual_level: i32) -> i32 {
-  let mut score = 0;
-  
-  score = (player_level * P_LEVEL_WEIGHT + health * P_HEALTH_WEIGHT) * actual_level;
-  
-  score.floor()
-}*/
 
+pub fn fitness(player_level: i32, health: i32, actual_level: i32) -> i32 {
+  let mut score = 0.0;
+  
+  score = (player_level as f32 * P_LEVEL_WEIGHT + health as f32 * P_HEALTH_WEIGHT) * actual_level as f32;
+  
+  score.floor() as i32
+}
+
+#[derive(Clone)]
+pub struct Gene {
+  weight: f32,
+}
+
+impl Gene {
+  pub fn new(weight: f32) -> Gene {
+    Gene {
+      weight: weight,
+    }
+  }
+  
+  pub fn new_random() -> Gene {
+    let mut rng = thread_rng();
+    Gene {
+      weight: rng.gen_range(0.0, 1.0),
+    }
+  }
+  
+  pub fn weight_value(&self) -> f32 {
+    self.weight
+  }
+}
+
+#[derive(Clone)]
+pub struct Genome {
+  num_connections_per_gene: u32,
+  genes: Vec<Gene>,
+  _total_value: f32,
+}
+
+impl Genome {
+  pub fn new(num_connections_per_gene: u32) -> Genome {
+    let mut new_genes = Vec::with_capacity(num_connections_per_gene as usize);
+    for i in 0..num_connections_per_gene {
+      new_genes.push(Gene::new_random());
+    }
+    
+    Genome {
+      num_connections_per_gene: num_connections_per_gene,
+      genes: new_genes,
+      _total_value: 0.0,
+    }
+  }
+  
+  pub fn recieve_input(&mut self, other: Genome) {
+    // summation()
+    // activation()
+  }
+  
+  pub fn summation(&mut self, other: Genome) -> f32 {
+    // inputs weighted and added up
+    0.0
+  }
+  
+  pub fn activation(&mut self, summation: f32) -> f32 {
+    // 1 / (1 + e^(-x))
+    0.0
+  }
+  
+  pub fn print_weights(&self) {
+    for i in 0..self.num_connections_per_gene as usize {
+      print!("{}, ", self.genes[i].weight_value());
+    }
+    println!("");
+  }
+}
+
+#[derive(Clone)]
+pub struct Layers {
+  num_input_nodes: u32,
+  num_output_nodes: u32,
+  num_hidden_nodes: u32,
+  num_hidden_layers: u32,
+  input_layer: Vec<Genome>,
+  output_layer: Vec<Genome>,
+  hidden_layers: Vec<Vec<Genome>>,
+}
+
+impl Layers {
+  pub fn new(num_input_nodes: u32, num_output_nodes: u32, num_hidden_nodes: u32, num_hidden_layers: u32,) -> Layers {
+    let mut input_layer = Vec::with_capacity(num_input_nodes as usize);
+    let mut hidden_layers = Vec::with_capacity(num_hidden_layers as usize);
+    let mut output_layer = Vec::with_capacity(num_output_nodes as usize);
+    
+    for i in 0..num_input_nodes {
+      input_layer.push(Genome::new(0));
+    }
+    for i in 0..num_hidden_layers as usize {
+      hidden_layers.push(Vec::with_capacity(num_input_nodes as usize));
+      for j in 0..num_hidden_nodes as usize {
+        hidden_layers[i].push(Genome::new(num_input_nodes));
+      }
+    }
+    for i in 0..num_output_nodes {
+      output_layer.push(Genome::new(num_hidden_nodes));
+    }
+    
+    Layers {
+      num_input_nodes: num_input_nodes,
+      num_output_nodes: num_output_nodes,
+      num_hidden_nodes: num_hidden_nodes,
+      num_hidden_layers: num_hidden_layers,
+      input_layer: input_layer,
+      output_layer: output_layer,
+      hidden_layers: hidden_layers,
+    }
+  }
+  
+  pub fn print_weights(&self) {
+    for i in 0..self.num_input_nodes as usize {
+      self.input_layer[i].print_weights();
+    }
+    for i in 0..self.num_hidden_layers as usize {
+      for j in 0..self.num_hidden_nodes as usize {
+        self.hidden_layers[i][j].print_weights();
+      }
+    }
+    for i in 0..self.num_output_nodes as usize {
+      self.output_layer[i].print_weights();
+    }
+  }
+}
+
+pub struct Population {
+  generation: i32,
+  population_size: u32,
+  population: Vec<Layers>,
+  best_fitness: f32,
+  best_layer: Layers,
+}
+
+impl Population {
+  pub fn new(population_size: u32) -> Population {
+    let generation = 0;
+    let best_fitness = 0.0;
+    
+    let num_input_nodes = 7;
+    let num_output_nodes = 11;
+    let num_hidden_nodes = num_input_nodes*2;
+    let num_hidden_layers = 1;
+    
+    let mut population = Vec::with_capacity(population_size as usize);
+    for i in 0..population_size as usize {
+      population.push(Layers::new(num_input_nodes, num_output_nodes, num_hidden_nodes, num_hidden_layers));
+    }
+    let best_layer = population[0].clone();
+    
+    Population {
+      generation: generation,
+      population_size: population_size,
+      population: population,
+      best_fitness: best_fitness,
+      best_layer: best_layer,
+    }
+  }
+  
+  pub fn print_best_fitness_weights(&self) {
+    self.best_layer.print_weights();
+  }
+}
 
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,61 @@ extern crate enigo;
 extern crate image;
 extern crate screenshot;
 extern crate bmp;
+extern crate rand;
 
 mod GeneticAlgorithm;
+
 use screenshot::get_screenshot;
 use bmp::{Image, Pixel};
 
 use std::process::{Stdio, Command};
 
 fn main() {
-	let s = get_screenshot(0).unwrap();
-
-	println!("{} x {} x {} = {} bytes", s.height(), s.width(), s.pixel_width(), s.raw_len());
-
-	let origin = s.get_pixel(0, 0);
-	println!("(0,0): R: {}, G: {}, B: {}", origin.r, origin.g, origin.b);
-
-	let end_col = s.get_pixel(0, s.width()-1);
-	println!("(0,end): R: {}, G: {}, B: {}", end_col.r, end_col.g, end_col.b);
-
-	let opp = s.get_pixel(s.height()-1, s.width()-1);
-	println!("(end,end): R: {}, G: {}, B: {}", opp.r, opp.g, opp.b);
-
-	// WARNING rust-bmp params are (width, height)
-	let mut img = Image::new(s.width() as u32, s.height() as u32);
-	for row in (0..s.height()) {
-		for col in (0..s.width()) {
-			let p = s.get_pixel(row, col);
-			// WARNING rust-bmp params are (x, y)
-			img.set_pixel(col as u32, row as u32, Pixel {r: p.r, g: p.g, b: p.b});
-		}
-	}
-	img.save("test.bmp").unwrap();
-
-	image::save_buffer("test.png",
-		s.as_ref(), s.width() as u32, s.height() as u32, image::RGBA(8))
-	.unwrap();
-
-	let output = if cfg!(target_os = "windows") {
+  let s = get_screenshot(0).unwrap();
+  
+  println!("{} x {} x {} = {} bytes", s.height(), s.width(), s.pixel_width(), s.raw_len());
+  
+  let origin = s.get_pixel(0, 0);
+  println!("(0,0): R: {}, G: {}, B: {}", origin.r, origin.g, origin.b);
+  
+  let end_col = s.get_pixel(0, s.width()-1);
+  println!("(0,end): R: {}, G: {}, B: {}", end_col.r, end_col.g, end_col.b);
+  
+  let opp = s.get_pixel(s.height()-1, s.width()-1);
+  println!("(end,end): R: {}, G: {}, B: {}", opp.r, opp.g, opp.b);
+  
+  // WARNING rust-bmp params are (width, height)
+  let mut img = Image::new(s.width() as u32, s.height() as u32);
+  for row in (0..s.height()) {
+    for col in (0..s.width()) {
+      let p = s.get_pixel(row, col);
+      // WARNING rust-bmp params are (x, y)
+      img.set_pixel(col as u32, row as u32, Pixel {r: p.r, g: p.g, b: p.b});
+    }
+  }
+  img.save("test.bmp").unwrap();
+  
+  image::save_buffer("test.png",
+    s.as_ref(), s.width() as u32, s.height() as u32, image::RGBA(8))
+  .unwrap();
+  
+  let pop_size = 10;
+  let mut the_brain = GeneticAlgorithm::brain::Population::new(pop_size);
+  the_brain.print_best_fitness_weights();
+  
+  let output = if cfg!(target_os = "windows") {
     Command::new(r#"\Users\samue\Documents\projects\NuclearThrone\nuclearthrone.exe"#)
-			.stdin(Stdio::piped())
+      .stdin(Stdio::piped())
             .output()
             .expect("failed to execute process")
-	} else {
-	 Command::new("sh")
+  } else {
+    Command::new("sh")
             .arg("-c")
             .arg("../../Games/nuclearthrone/runner")
             .output()
             .expect("failed to execute process")
-	};
-	
-	println!("{:?}", output);
+  };
+  
+  println!("{:?}", output);
 }
 
