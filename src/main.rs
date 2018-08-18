41d8fca8c0684b70888aa1313e1290c92a71355e
Lilith645 <lilith@inet-sys.com>
2018-08-18 17:35:45 +1000

    added system call library Changes committed: 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-//extern crate enigo;
-//extern crate screenshot;
+extern crate enigo;
 extern crate image;
 
+#[macro_use]
+extern crate syscall;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  unsafe {
+    syscall!(_SYSCTL, "gnome_screenshot".as_ptr());
+  }
 }
