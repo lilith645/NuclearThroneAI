504384ad0b07b6a1cb35f6197ea82f3f90437779
Lilith645 <lilith@inet-sys.com>
2018-08-18 23:58:27 +1000

    stuffs with the screenshot rs Changes committed: 	new file:   .gitmodules 	new file:   screenshot-rs 	modified:   src/GeneticAlgorithm/brain.rs

--- src/GeneticAlgorithm/brain.rs
+++ src/GeneticAlgorithm/brain.rs
@@ -1,5 +1,6 @@
-pub const P_LEVEL_WEIGHT: f32 =  1;
-pub const G_LEVEL_WEIGHT: f32 = 0.5;
+//pub const P_LEVEL_WEIGHT: f32 =  1;
+//pub const G_LEVEL_WEIGHT: f32 = 0.5;
+
 
 pub fn fitness(player_level: i32, health: i32, actual_level: i32) {
   let mut score = 0;
